0c783ff1a095dc2fbfcdb990da5a01e2fa9a5213
Connor Abdelnoor & Jill Kuchman <student@epicodus.com>
2015-03-10 14:55:07 -0700

    Add form to app page.

--- app/app.php
+++ app/app.php
@@ -2,6 +2,17 @@
     require_once __DIR__."/../vendor/autoload.php";
     require_once __DIR__."/../src/AnagramGenerator.php";
 
+    $app = new Silex\Application();
+
+    $app->register(new Silex\Provider\TwigServiceProvider(), array(
+        'twig.path' => __DIR__.'/../views'
+    ));
+
+    $app->get("/", function() use($app){
+        return $app['twig']->render('form.twig');
+    });
+
+
 
 
 
