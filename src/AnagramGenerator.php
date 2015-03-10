d7b22c497e6737d10df397e13dd0527dd6e66c1c
Connor Abdelnoor & Jill Kuchman <student@epicodus.com>
2015-03-10 13:32:18 -0700

    match rearranged letters to the anagram input if they match in length.

--- src/AnagramGenerator.php
+++ src/AnagramGenerator.php
@@ -1,21 +1,46 @@
 <?php
     class Anagram {
 
+        // function createAnagram($anagram, $list){
+        //     //if word in $list matches $anagram, add the word in $list to an array $outputarray
+        //
+        //     $listArray = explode(", ",$list);
+        //     $outputArray = array();
+        //     foreach ($listArray as $listItem)
+        //     {
+        //         if ($listItem == $anagram)
+        //         {
+        //             array_push($outputArray, $listItem);
+        //         }
+        //     }
+        //     return implode(", ", $outputArray);
+        //
+        //
+        // }
+
         function createAnagram($anagram, $list){
             //if word in $list matches $anagram, add the word in $list to an array $outputarray
 
-            $listArray = explode(", ",$list);
+            $listArray = explode(", ", $list);
+
+            $anaArray = str_split($anagram);
+            sort($anaArray);
             $outputArray = array();
-            foreach ($listArray as $listItem)
-            {
-                if ($listItem == $anagram)
-                {
+
+            foreach($listArray as $listItem){
+                $split_Item = str_split($listItem);
+                sort($split_Item);
+
+
+
+                if($split_Item == $anaArray){
+
                     array_push($outputArray, $listItem);
                 }
             }
+
             return implode(", ", $outputArray);
 
-            
         }
     }
 
--- tests/AnagramGeneratorTest.php
+++ tests/AnagramGeneratorTest.php
@@ -14,8 +14,51 @@
             $result = $test_AnagramGenerator->createAnagram($input_anagram, $input_list);
 
             //Assert
-            
+
             $this->assertEquals("a" , $result);
+        }
+
+        function test_CreateAnagram_oneLetterFalse(){
+            $test_AnagramGenerator = new Anagram;
+            $input_anagram = "a";
+            $input_list = "b";
+
+            $result = $test_AnagramGenerator->createAnagram($input_anagram, $input_list);
+
+            $this->assertEquals("", $result);
+        }
+
+        function test_CreateAnagram_multipleSingle(){
+            //Arrange
+            $test_AnagramGenerator = new Anagram;
+            $input_anagram = "a";
+            $input_list = "a, b";
+            //Act
+            $result = $test_AnagramGenerator->createAnagram($input_anagram, $input_list);
+            //Assert
+            $this->assertEquals("a", $result);
+        }
+
+        function test_CreateAnagram_WordArrangement(){
+            //Arrange
+            $test_AnagramGenerator = new Anagram;
+            $input_anagram = "he";
+            $input_list = "he, ho";
+            //Act
+            $result = $test_AnagramGenerator->createAnagram($input_anagram, $input_list);
+            //Assert
+            $this->assertEquals("he", $result);
+        }
+
+        function test_CreateAnagram_WordRearrange(){
+            //Arrange
+            $test_AnagramGenerator = new Anagram;
+            $input_anagram = "he";
+            $input_list = "he, ho, eh";
+            //Act
+            $result = $test_AnagramGenerator->createAnagram($input_anagram, $input_list);
+            //Assert
+            $this->assertEquals("he, eh", $result);
         }
 
 
